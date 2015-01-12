ckeditor/ckeditor4

test for passing startup data from widget command

tests/plugins/widget/definition.js
@@ -426,6 +426,26 @@
 			} );
 		},
 
+		'test command with startup data': function() {
+			var editor = this.editor,
+				executed = 0;
+
+			var widgetDef = {
+				data: function (event) {
+					executed += 1;
+					assert.areSame( 2, event.data.bar, 'startup data was passed' );
+				},
+				template: '<span>data</span>'
+			};
+
+			editor.widgets.add( 'testcommanddata', widgetDef );
+
+			this.editorBot.setData( '<p>foo</p>', function() {
+				editor.execCommand( 'testcommanddata', { startupData: { bar: 2 } } );
+				assert.areSame( 1, executed, 'data listener was executed once' );
+			} );
+		},
+
 		'test insert method': function() {
 			var editor = this.editor,
 				insertExecuted = 0,

yandex-ui/ckeditor

src/ckeditor/tests/plugins/widget/definition.js
@@ -426,6 +426,26 @@
 			} );
 		},
 
+		'test command with startup data': function() {
+			var editor = this.editor,
+				executed = 0;
+
+			var widgetDef = {
+				data: function( evt ) {
+					executed += 1;
+					assert.areSame( 2, evt.data.bar, 'startup data was passed' );
+				},
+				template: '<span>data</span>'
+			};
+
+			editor.widgets.add( 'testcommanddata', widgetDef );
+
+			this.editorBot.setData( '<p>foo</p>', function() {
+				editor.execCommand( 'testcommanddata', { startupData: { bar: 2 } } );
+				assert.areSame( 1, executed, 'data listener was executed once' );
+			} );
+		},
+
 		'test insert method': function() {
 			var editor = this.editor,
 				insertExecuted = 0,