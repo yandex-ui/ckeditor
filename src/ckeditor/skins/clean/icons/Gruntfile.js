9f20788d2f44d81fdc605db73c06d15837d87423
Katochimoto <bufpost@yandex.ru>
2015-09-01 23:09:18 +0300

    Initial commit

--- src/ckeditor/skins/clean/icons/Gruntfile.js
+++ src/ckeditor/skins/clean/icons/Gruntfile.js
@@ -2,19 +2,19 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('svg_fallback');
 
     grunt.initConfig({
-        pkg: grunt.file.readJSON('../package.json'),
-        svg_fallback: {
-            options: {
-                //debug: true,
-                svgclass: 'svgicon',
-                closetags: false,
-                movestyles: true,
-                // usei8class: true,
-                //svgstyle: "pointer-events: none; visibility: hidden;"
+        'pkg': grunt.file.readJSON(__dirname + '/../package.json'),
+        'svg_fallback': {
+            'options': {
+                //'debug': true,
+                'svgclass': 'svgicon',
+                'closetags': false,
+                'movestyles': true,
+                //'usei8class': true,
+                //'svgstyle': 'pointer-events: none; visibility: hidden;'
             },
-            your_target: {
-                src: 'src/',
-                dest: 'out/'
+            'your_target': {
+                'src': 'src/',
+                'dest': 'out/'
             }
         }
     });
