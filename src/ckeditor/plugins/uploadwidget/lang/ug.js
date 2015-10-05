6d81af87af0968dba8b449aa1581ae87000dbf9e
Wiktor Walc <w.walc@cksource.com>
2015-10-05 10:08:06 +0200

    Updated language files.

--- src/ckeditor/plugins/uploadwidget/lang/ug.js
+++ src/ckeditor/plugins/uploadwidget/lang/ug.js
@@ -5,8 +5,13 @@
 
 CKEDITOR.plugins.setLang( 'uploadwidget', 'ug', {
 	abort: 'يۈكلەشنى ئىشلەتكۈچى ئۈزۈۋەتتى.',
+<<<<<<< HEAD
 	doneOne: 'ھۆججەت مۇۋەپپەقىيەتلىك يۈكلەندى.',
 	doneMany: 'مۇۋەپپەقىيەتلىك ھالدا %1 ھۆججەت يۈكلەندى.',
+=======
+	doneOne: 'File successfully uploaded.', // MISSING
+	doneMany: 'Successfully uploaded %1 files.', // MISSING
+>>>>>>> 144e2c2... Updated language files.
 	uploadOne: 'Uploading file ({percentage}%)...', // MISSING
 	uploadMany: 'Uploading files, {current} of {max} done ({percentage}%)...' // MISSING
 } );
