7bc7fc3781f5a6e7b8f7cad387b20ee637a14488
Katochimoto <bufpost@yandex.ru>
2016-06-01 20:11:44 +0300

    up skin clean

--- dist/ckeditor/ckeditor.js
+++ dist/ckeditor/ckeditor.js
@@ -62,7 +62,7 @@ if ( !window.CKEDITOR ) {
 			 *
 			 *		alert( CKEDITOR.revision ); // e.g. '3975'
 			 */
-			revision: '28a09a3',
+			revision: 'a255c4d',
 
 			/**
 			 * A 3-digit random integer, valid for the entire life of the CKEDITOR object.
